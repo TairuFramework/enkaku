84676117fc4a0ab73d138f7eaa58d069c754df53
Paul Le Cam <paul@ulem.net>
2025-09-17 22:33:11 +0100

    Add Expo E2E tests

---
+++ tests/e2e-expo/App.tsx
@@ -0,0 +1,54 @@
+import { provideTokenSigner } from '@enkaku/expo-keystore'
+import { type SignedToken, type Token, verifyToken } from '@enkaku/token'
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+
+type Data = {
+  test: string
+}
+
+export default function App() {
+  const [signer] = useState(() => provideTokenSigner('test'))
+  const [signedToken, setSignedToken] = useState<SignedToken<Data> | null>(null)
+  const [verifiedToken, setVerifiedToken] = useState<Token<Data> | null>(null)
+
+  let button = null
+  if (signedToken == null) {
+    button = (
+      <Button
+        title="Sign token"
+        onPress={() => {
+          signer.createToken({ test: 'OK' }).then(setSignedToken)
+        }}
+      />
+    )
+  } else if (verifiedToken == null) {
+    button = (
+      <Button
+        title="Verify token"
+        onPress={() => {
+          verifyToken(signedToken).then(setVerifiedToken)
+        }}
+      />
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      {button}
+      {verifiedToken ? <Text>Verified token: {verifiedToken.payload.test}</Text> : null}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+})
