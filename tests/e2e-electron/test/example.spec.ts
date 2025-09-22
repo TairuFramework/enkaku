6b526a01c690696b8268e0ae89903b08dc5de798
Paul Le Cam <paul@ulem.net>
2025-09-22 19:36:35 +0100

    Setup desktop workflow

--- tests/e2e-electron/test/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { _electron as electron, test } from '@playwright/test'
-
-test('launch app', async () => {
-  const electronApp = await electron.launch({ args: ['.vite/build/main.js'] })
-  await electronApp.close()
-})
