140aaa190cb2262d8a70517f3e8bae8492d559f5
Paul Le Cam <paul@ulem.net>
2025-09-24 21:15:40 +0100

    Setup Web E2E tests

---
+++ tests/e2e-web/src/App.tsx
@@ -0,0 +1,52 @@
+import { provideTokenSigner } from '@enkaku/browser-keystore'
+import { type Token, verifyToken } from '@enkaku/token'
+import { useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+
+const signerPromise = provideTokenSigner('test')
+
+type Data = {
+  test: string
+}
+
+export default function App() {
+  const [signedToken, setSignedToken] = useState<Token<Data> | null>(null)
+  const [verifiedToken, setVerifiedToken] = useState<Token<Data> | null>(null)
+
+  let button = null
+  if (signedToken == null) {
+    button = (
+      <Button
+        title="Sign token"
+        onPress={() => {
+          signerPromise.then((signer) => signer.createToken({ test: 'OK' })).then(setSignedToken)
+        }}
+      />
+    )
+  } else if (verifiedToken == null) {
+    button = (
+      <Button
+        title="Verify token"
+        onPress={() => {
+          verifyToken(signedToken).then(setVerifiedToken)
+        }}
+      />
+    )
+  }
+  return (
+    <View style={styles.container}>
+      {button}
+      {verifiedToken ? <Text>Verified token: {verifiedToken.payload.test}</Text> : null}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+})
---
+++ tests/e2e-web/vite.config.ts
@@ -0,0 +1,36 @@
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    DEV: process.env.NODE_ENV === 'development' ? 'true' : 'false',
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      resolveExtensions: [
+        '.web.js',
+        '.web.jsx',
+        '.web.ts',
+        '.web.tsx',
+        '.mjs',
+        '.js',
+        '.mts',
+        '.ts',
+        '.jsx',
+        '.tsx',
+        '.json',
+      ],
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+    },
+  },
+})
